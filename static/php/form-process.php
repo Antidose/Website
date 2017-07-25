99f48aba7eadfb14f10d21a92d060957ad3a4138
Heather Cape <hcape@uvic.ca>
2017-07-25 15:17:29 -0700

    add pics of the app

--- static/php/form-process.php
+++ static/php/form-process.php
@@ -32,7 +32,7 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "armanmia7@gmail.com";
+$EmailTo = "heather.magic.cape@gmail.com";
 $Subject = "New Message Received";
 
 // prepare email body text
